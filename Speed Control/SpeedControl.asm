12585d621afedc865598772ca5def8f38f266980
Zezyn <7607920511m.h@gmail.com>
2016-04-22 20:08:57 -0700

    organizing

--- Speed Control/SpeedControl.asm
+++ Speed Control/SpeedControl.asm
@@ -9,10 +9,13 @@
 .list
 
 .def TEMP = R16
-.def COUNT = R17
+.def SHIFTreg = R17
 .def UpDown = R18
+.def SPEED = R19
+.equ DBcount = 100
 
 .equ TESTvalue = $0A63
+.equ LeftRight = 7
 
 .ORG	$0000
 		rjmp 	RESET
@@ -21,16 +24,16 @@
 .ORG	INT1addr													;Interrupt Vector for External Interrupt 1
 		rjmp	INT1isr
 .ORG	OVF0addr
-		rjmp 	CountUpDown
+		rjmp 	ROTATE
 
 .ORG	INT_VECTORS_SIZE
-
 
 RESET:
 		ldi		TEMP, low(RAMEND)									;Load low byte of last address for SRAM into R16
 		out		SPL, TEMP											;Output value in TEMP (R16) to Stack Pointer Low Byte.
 
-		ldi		TEMP, (1<<DDB3)|(1<<DDB2)|(1<<DDB1)|(1<<DDB0)	;PortB Data Direction
+		//ldi		TEMP, (1<<DDB3)|(1<<DDB2)|(1<<DDB1)|(1<<DDB0)		;PortB Data Direction
+		ldi  	TEMP, $FF
 		out 	DDRB, TEMP											;Output value in TEMP to Data Direfction Register for Port B
 
 		ldi		TEMP, $00
@@ -51,7 +54,7 @@ RESET:
 		ldi 	TEMP, (1<<CS02)|(0<<CS01)|(1<<CS00)
 		out		TCCR0B, TEMP		
 
-		clr 	COUNT 												;Clear all bits in COUNT. COUNT = 0
+		ldi 	SHIFTreg, 1 												;Clear all bits in COUNT. COUNT = 0
 
 		clr 	UpDown												;Clears UpDown register
 
@@ -66,18 +69,49 @@ MAIN:
 		rjmp	MAIN
 
 INT0isr:															;Inturrupt 0 routine
-		ldi		Updown, $01
-		nop
+		sbis 	GPIOR0, LeftRight
+		rjmp	Goleft
+		cbi		GPIOR0,	LeftRight
+		//ldi		Updown, $01
+		//nop
+		//reti
+		//sbi 	GPIOR0, LeftRight
+		reti
+
+Goleft:
+		sbi	GPIOR0, LeftRight
 		reti
 
 INT1isr:															;Interrupt 1 routine
-		ldi		UpDown, $FF
-		nop
+		//ldi		UpDown, $FF
+		//nop
+		//reti
+		//cbi		GPIOR0, LeftRight
+		//reti
+		DEBOUNCE PIND,(1<<PD2), 10
+		ldi		TEMP, $20
+		add		SPEED, TEMP
+		reti
+ROTATE:
+		out		TCNT0, SPEED
+		sbis	GPIOR0, LeftRight
+		rjmp	LEFT
+		bst		SHIFTreg, 0
+		lsr		SHIFTreg
+		bld		SHIFTreg, 7
+		out		PORTB, SHIFTreg
 		reti
 
-CountUpDown:														;Count routine
+LEFT:
+		bst		SHIFTreg, 7
+		lsl		SHIFTreg
+		bld		SHIFTreg, 0
+		out 	PORTB, SHIFTreg
+		reti
+			
+/*CountUpDown:														;Count routine
 		add		COUNT, UpDown 										;Adds two regesters together
 		out		PORTB, COUNT
 		reti
-
+*/
 
